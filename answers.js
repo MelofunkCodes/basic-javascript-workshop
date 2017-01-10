b338b0834d41ea86dc232f9a4197a6ec877506cb
MelofunkCodes <myeoh27@gmail.com>
2017-01-10 19:27:21 +0000

    Added firstCharacter function

---
+++ answers.js
@@ -0,0 +1,8 @@
+//function will return first character of a string
+function firstCharacter(word) {
+    return word[0];
+}
+
+console.log( firstCharacter("dog") );
+console.log (firstCharacter("supercalifragilisticexpialadocious") );
+console.log ( firstCharacter(" ") );
