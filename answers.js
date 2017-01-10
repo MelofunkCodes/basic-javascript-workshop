69708bcbb00fd377054201c6fea8d4bca22feda0
MelofunkCodes <myeoh27@gmail.com>
2017-01-10 21:05:03 +0000

    Added oneToTenWords function

--- answers.js
+++ answers.js
@@ -97,3 +97,53 @@ function repeatWord (word, number) {
 console.log("\nREPEAT function: ");
 console.log( repeatWord("dog", 2) );
 console.log( repeatWord("tunnup", 16) );
+
+//function will not take anything, but just print out numbers 1 to 10 inclusively
+function oneToTen() {
+    for (var i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+console.log("Print 1 to 10 function: ");
+oneToTen();
+
+//function will print numbers 1 to 10 as words
+function oneToTenWords() {
+    for (var i = 1; i<= 10; i++) {
+        switch(i) {
+            case 1:
+                console.log("one");
+                break;
+            case 2:
+                console.log("two");
+                break;
+            case 3:
+                console.log("three");
+                break;
+            case 4:
+                console.log("four");
+                break;
+            case 5:
+                console.log("five");
+                break;
+            case 6:
+                console.log("six");
+                break;
+            case 7:
+                console.log("seven");
+                break;
+            case 8:
+                console.log("eight");
+                break;
+            case 9:
+                console.log("nine");
+                break;
+            default:
+                console.log("ten");
+        }
+    }
+}
+
+console.log("\nPrint 1 to 10 function (in word form): ");
+oneToTenWords();
