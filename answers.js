cdf4a1fc01910a10abe1092cf4841ff345f2dab1
MelofunkCodes <myeoh27@gmail.com>
2017-01-10 19:42:06 +0000

    Added lastCharacter function

--- answers.js
+++ answers.js
@@ -3,6 +3,18 @@ function firstCharacter(word) {
     return word[0];
 }
 
+console.log("FIRST CHARACTER FUNCTION: ");
 console.log( firstCharacter("dog") );
 console.log (firstCharacter("supercalifragilisticexpialadocious") );
 console.log ( firstCharacter(" ") );
+
+
+//function will return last character of a string
+function lastCharacter(word) {
+    return word[word.length - 1];
+}
+
+console.log("LAST CHARACTER FUNCTION: ");
+console.log( lastCharacter("Mr. Bean") );
+console.log( lastCharacter("Mexico") );
+console.log( lastCharacter("Crunchy chips ") );
